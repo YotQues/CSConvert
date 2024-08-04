1040b1074dfc633af3adf3467267e614cbeda620
YotamAran <yotamaran@applydesign.io>
2024-08-04 13:20:44 +0300

    Refactored test class to have a base and inheritors, DemoTypes in seperate file

---
+++ CSConvert.Tests/CodeConverterTestBase.cs
@@ -0,0 +1,21 @@
+﻿using CSConvert.Abstractions;
+using CSConvert.TypeScript;
+
+namespace CSConvert.Tests
+{
+    public abstract class CodeConverterTestBase
+    {
+        protected abstract Type[] types { get; set; }
+        protected abstract ICodeConverter converter { get; }
+
+        protected void TestConverter(Type type, string expected)
+        {
+            var processedTypes = new HashSet<string>();
+
+            string actual = converter.Convert(types.First(t => t.Name == type.Name)).Trim();
+
+            Assert.Equal(expected.Trim(), actual);
+        }
+
+    }
+}
